26ff7ebc532f0c4f1dfb276013c2eea75f35244c
Juan Camilo Capera <capera.juan@correounivalle.edu.co>
2023-04-23 02:45:51 -0500

    Creando clase Principal

---
+++ src/Vista/Principal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vista;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author juan-
+ */
+public class Principal {
+    public static void main(String[] args) {
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);         
+    }
+}
