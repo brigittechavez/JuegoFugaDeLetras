1ded4a9b242b5989ae5a504edc6fd4174fb070e7
Juan Camilo Capera <capera.juan@correounivalle.edu.co>
2023-04-23 14:44:18 -0500

    Inicializando los componentes

--- src/Vista/VentanaRegistro.java
+++ src/Vista/VentanaRegistro.java
@@ -4,10 +4,66 @@
  */
 package Vista;
 
+import Modelo.Jugador;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+
 /**
  *
  * @author juan-
  */
-public class VentanaRegistro {
+public final class VentanaRegistro extends JFrame {
+    private JLabel jlMensaje;
+    private JPanel jpContenido;
+    private JLabel jlNombre;
+    private JLabel jlTematica;
+    private JTextField txtNombre;
+    private JButton btnAnimal;
+    private JButton btnColor;
+    private JButton btnFruta;
+    
+    public VentanaRegistro(){
+        iniciarComponentes();
+    }
+    
+    private void iniciarComponentes(){
+        //Se configura la ventana con un tamaño acorde
+        setTitle("Fuga de Letras");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);;
+        setSize(600,500);
+        setLocationRelativeTo(null);
+        setVisible(true); 
+        setResizable(false);
+        
+        // Crear el panel que contendrá el fondo
+        FondoPanel panelFondo = new FondoPanel("/Imagenes/Fondo.jpg");
+        panelFondo.setBounds(0, 0, 600, 500);
+        panelFondo.setOpaque(false);
+
+        // Agregar el panel de fondo al panel principal
+        add(panelFondo);
+        
+        Toolkit miPantalla = Toolkit.getDefaultToolkit();
+        
+        //CONFIGURANDO LOS MENSAJES EN PANTALLA
+        jlMensaje = new JLabel("Ahora debes realizar el registro",SwingConstants.CENTER);
+        jlNombre = new JLabel("Ingresa tu nombre",SwingConstants.CENTER );
+        jlTematica = new JLabel("Selecciona una temática",SwingConstants.CENTER );
+    }
     
 }
