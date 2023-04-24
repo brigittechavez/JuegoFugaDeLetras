c96cae78bdd8966fb4ee5f62e15a51d0fd03a6ea
Brigitte Chavez <brigitte.chavez@correounivalle.edu.co>
2023-04-23 19:49:34 -0500

    Función para recorrer .txt y almacenar. Creación Get & Set

--- src/Modelo/Categoría.java
+++ src/Modelo/Categoría.java
@@ -4,10 +4,67 @@
  */
 package Modelo;
 
+import java.io.BufferedReader; 
+import java.io.FileReader; 
+import java.io.IOException;
+import java.util.ArrayList;
 /**
  *
- * @author Vanesa
+ * @author Vanesa 
  */
 public class Categoría {
+    private ArrayList<String> Animales = new ArrayList <String> ();
+    private ArrayList<String> Colores = new ArrayList <String> ();
+    private ArrayList<String> Frutas = new ArrayList <String> ();
     
+    
+    //Función encargada de leer las palabras de los archivos .txt
+    public Categoría() {
+        leerArchivo("Animales.txt", Animales); 
+        leerArchivo("Colores.txt", Colores); 
+        leerArchivo("Frutas.txt", Frutas); 
+    }
+    
+    //Función encargada de guardar las palabras en las categorías 
+    
+    private void leerArchivo(String nombreArchivo, ArrayList<String> listaPalabras) {
+        try (BufferedReader br = new BufferedReader(new FileReader(nombreArchivo))) { 
+            String palabraTemp; // Variable temporal
+            while ((palabraTemp = br.readLine()) != null) { 
+                listaPalabras.add(palabraTemp); // Agrega la palabra leída a su categoría correspondiente
+            }
+        } catch (IOException e) {  
+            
+        }
+    }
+
+    //Getters & Setters 
+    
+    public ArrayList<String> getAnimales() {
+        return Animales;
+    }
+
+    public void setAnimales(ArrayList<String> Animales) {
+        this.Animales = Animales;
+    }
+
+    public ArrayList<String> getColores() {
+        return Colores;
+    }
+
+    public void setColores(ArrayList<String> Colores) {
+        this.Colores = Colores;
+    }
+
+    public ArrayList<String> getFrutas() {
+        return Frutas;
+    }
+
+    public void setFrutas(ArrayList<String> Frutas) {
+        this.Frutas = Frutas;
+    }
+    
+    
+    
+       
 }
