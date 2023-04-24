f13319138a1b407b35a17d0d242ba78803953bb7
Juan Camilo Capera <capera.juan@correounivalle.edu.co>
2023-04-23 23:17:57 -0500

    TERMINANDO LA CONFIGURACIÓN DE LOA COMPONENTES

--- src/Vista/VentanaJuego.java
+++ src/Vista/VentanaJuego.java
@@ -82,7 +82,7 @@ public class VentanaJuego extends JFrame {
         jpContenido = new JPanel();
         
         jlJugador = new JLabel("Jugador : ");
-        jlJugador.setBounds(5,110, 519,35);
+        jlJugador.setBounds(20,7, 200,35);
         jlJugador.setForeground(Color.BLACK);
         jlJugador.setFont(new Font("arial", Font.BOLD, 20));
         
@@ -90,62 +90,94 @@ public class VentanaJuego extends JFrame {
         nombre =    nombre.substring(0,1).toUpperCase() + 
                     nombre.substring(1).toLowerCase();
         jlNombreJugador = new JLabel(nombre);
-        jlNombreJugador.setBounds(100,110, 519,35);
+        jlNombreJugador.setBounds(100,7, 519,35);
         jlNombreJugador.setForeground(Color.BLACK);
         jlNombreJugador.setFont(new Font("arial", Font.BOLD, 20));
         
-        jlIntento = new JLabel("Palabra # : ");
-        jlIntento.setBounds(230,110, 519,35);
-        jlIntento.setForeground(Color.BLACK);
-        jlIntento.setFont(new Font("arial", Font.BOLD, 20));
+        jlTotalPalabras = new JLabel("Palabra # : ");
+        jlTotalPalabras.setBounds(230,70, 519,35);
+        jlTotalPalabras.setForeground(Color.BLACK);
+        jlTotalPalabras.setFont(new Font("arial", Font.BOLD, 20));
                 
         jlNumeroPalabra = new JLabel(
                 String.valueOf(controladorJuego.getNumeroPalabra()));
-        jlNumeroPalabra.setBounds(330,110, 519,35);
+        jlNumeroPalabra.setBounds(330,70, 519,35);
         jlNumeroPalabra.setForeground(Color.BLACK);
         jlNumeroPalabra.setFont(new Font("arial", Font.BOLD, 20));
     
         jlIntento = new JLabel("Intento # : ");
-        jlIntento.setBounds(375,110, 519,35);
+        jlIntento.setBounds(375,7, 519,35);
         jlIntento.setForeground(Color.BLACK);
         jlIntento.setFont(new Font("arial", Font.BOLD, 20));
         
         jlNumeroIntento = new JLabel(
                 String.valueOf(controladorJuego.getIntentosPartida()));
-        jlNumeroIntento.setBounds(475,110, 519,35);
+        jlNumeroIntento.setBounds(475,7, 519,35);
         jlNumeroIntento.setForeground(Color.BLACK);
         jlNumeroIntento.setFont(new Font("arial", Font.BOLD, 20));
         
         jlVocal = new JLabel("¿Cuál es la vocal que hace falta?",SwingConstants.CENTER );
-        jlVocal.setBounds(0,180, 519,35);
+        jlVocal.setBounds(35,260, 519,35);
         jlVocal.setForeground(new Color(0,87,193));
         jlVocal.setFont(new Font("arial", Font.BOLD, 20));
+        //PALABRA A MOSTRAR
+        jlPalabra = new JLabel("Palabra # : ");
+        jlPalabra.setBounds(230,170, 519,35);
+        jlPalabra.setForeground(Color.BLACK);
+        jlPalabra.setFont(new Font("arial", Font.BOLD, 30));
+        
         //BOTON TERMINAR
         btnTerminar = new JButton("Terminar");
-        btnTerminar.setBounds(350,400, 160,35);
+        btnTerminar.setBounds(70,400, 160,35);
         btnTerminar.setBackground(Color.YELLOW);
         btnTerminar.setForeground(Color.BLACK); // Establecer el texto en color 
         btnTerminar.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
         //BOTON A
         btnA = new JButton("A");
-        btnA.setBounds(350,400, 160,35);
+        btnA.setBounds(120,300, 50,35);
         btnA.setBackground(Color.YELLOW);
         btnA.setForeground(Color.BLACK); // Establecer el texto en color
         btnA.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
-        btnA = new JButton("A");
-        btnA.setBounds(350,400, 160,35);
-        btnA.setBackground(Color.YELLOW);
-        btnA.setForeground(Color.BLACK); // Establecer el texto en color
-        btnA.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
+        //BOTON E
+        btnE = new JButton("E");
+        btnE.setBounds(180,300, 50,35);
+        btnE.setBackground(Color.YELLOW);
+        btnE.setForeground(Color.BLACK); // Establecer el texto en color
+        btnE.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
+        //BOTON I
+        btnI = new JButton("I");
+        btnI.setBounds(240,300, 50,35);
+        btnI.setBackground(Color.YELLOW);
+        btnI.setForeground(Color.BLACK); // Establecer el texto en color
+        btnI.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
+        //BOTON O
+        btnO = new JButton("O");
+        btnO.setBounds(300,300, 50,35);
+        btnO.setBackground(Color.YELLOW);
+        btnO.setForeground(Color.BLACK); // Establecer el texto en color
+        btnO.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
+        //BOTON U
+        btnU = new JButton("U");
+        btnU.setBounds(360,300, 50,35);
+        btnU.setBackground(Color.YELLOW);
+        btnU.setForeground(Color.BLACK); // Establecer el texto en color
+        btnU.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
         
         jpContenido.add(jlJugador);
         jpContenido.add(jlNombreJugador);
         jpContenido.add(jlIntento);
         jpContenido.add(jlNumeroPalabra);
-        jpContenido.add(jlIntento);
         jpContenido.add(jlNumeroIntento);
         jpContenido.add(jlVocal);
+        jpContenido.add(jlTotalPalabras);
+        jpContenido.add(jlPalabra);
         jpContenido.add(btnTerminar);
+        jpContenido.add(btnA);
+        jpContenido.add(btnE);
+        jpContenido.add(btnI);
+        jpContenido.add(btnO);
+        jpContenido.add(btnU);
+        
         
         jpContenido.setSize(600,500);        
         jpContenido.setBounds(0,110, 519, 500);
